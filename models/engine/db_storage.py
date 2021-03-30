190d8970d3e92cc994de548b1a57ff148719f623
ecastan960 <ecastan1@eafit.edu.co>
2021-03-30 06:21:48 -0500

    05|Update db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """Database storage"""
+import os
+import models
+from sqlalchemy.orm import Session
+from models.base_model import BaseModel, Base
+from sqlalchemy import *
+
 
 class DBStorage():
     """class DBStorage"""
@@ -8,4 +14,23 @@ class DBStorage():
 
     def __init__(self):
         """Constructor"""
-        self.__engine
+        username = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST' )
+        database = os.getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                           .format(username, password, host, database),
+                           pool_pre_ping=True)
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+    
+    def all(self, cls=None):
+        """Query on the current database session
+
+        Args:
+            cls ([type], optional): [description]. Defaults to None.
+        """
+        if cls is None:
+
+
+
