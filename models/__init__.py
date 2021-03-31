e994c82e3bc6c3324163df2ce77ca31cb600d97c
ecastan960 <ecastan1@eafit.edu.co>
2021-03-30 21:06:08 -0500

    11|Revert commits

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,5 @@ from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 import os
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-    storage.reload()
-else:
-    storage = FileStorage()
-    storage.reload()
+storage = FileStorage()
+storage.reload()
