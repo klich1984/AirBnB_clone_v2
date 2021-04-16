65340d9bedac3a6d8c2efd8f14a2b693e63b0183
klich1984 <2239@holbertonschool.com>
2021-04-16 16:56:36 -0500

    01 | update script | correcting pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" Task 1 """
+"""  script that generates a .tgz archive """
 from fabric.api import local
 import datetime
 from os.path import getsize
 
 
 def do_pack():
+    """generates a .tgz archive from the contents of the web_static
+    folder of your AirBnB"""
     local('mkdir -p versions')
     dt = datetime.datetime.now()
     dt = dt.strftime('%Y%m%d%H%M%S')
     path = './versions/web_static_{}.tgz'.format(dt)
-    print (path)
-    print ('./versions/web_static_{}.tgz'.format(dt))
     try:
         local("tar -cvzf ./versions/web_static_{}.tgz web_static".format(dt))
         # sz = getsize(path)
-        print('web_static packed: versions/web_static_20170314233357.tgz -> {}Bytes'.format(getsize(path)))
+        print('web_static packed: versions/web_static_20170314233357.tgz\
+              -> {}Bytes'.format(getsize(path)))
     except Exception:
         return None
