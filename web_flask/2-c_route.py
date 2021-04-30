c67d1c2a5f4d8ba07642f172a6d2da2f28a763cc
klich1984 <2239@holbertonschool.com>
2021-04-30 15:17:06 -0500

    00 | add script | script running and good output

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """ display "Hello HBNB!" in the route "/" """
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb")
+def home_hbnb():
+    """  display “HBNB” in route "/hbnb" """
+    return("HBNB")
+
+
+@app.route("/c/<text>")
+def argument(text):
+    """ display “C ” followed by the value of the text variable """
+    text1 = text.replace('_', ' ')
+    return("C {}".format(text1))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
